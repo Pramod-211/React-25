b245f7cbf0a4f152504289b12bbd307002ba1de6
pramod-211 <12345678+Pramod-211@users.noreply.github.com>
2025-08-26 15:48:52 +0530

    new

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion'
-import { useTransition } from 'react';
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: 'spring', delay: 0.5 }
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: 'easeInOut' }
+  }
+};
+
+const nextVariants = {
+  hidden: {
+    x: '-100vw'
+  },
+  visible: {
+    x: 0,
+    transition: { type: 'spring', stiffness: 120 }
+  }
+}
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity
+    }
+  }
+}
 
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div className="base container"
-      initial={{ y: '-100vw' }}
-      animate={{ y: 0 }}
-      transition={{ type: 'spring', delay: .3, stiffness: 30 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
     >
-
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map(base => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
             <motion.li key={base} onClick={() => addBase(base)}
-              whileHover={{ scale: 1.3, originX: 0, color: 'Yellow' }}
-              transition={{ type: 'spring', stiffness: 300 }}>
+              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
               <span className={spanClass}>{base}</span>
             </motion.li>
           )
@@ -28,21 +66,20 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <div className="next">
+        <motion.div className="next"
+          variants={nextVariants}
+        >
           <Link to="/toppings">
             <motion.button
-              initial={{ x: '-100vh', opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ type: 'spring', stiffness: 120 }}
-              whileHover={{ scale: 1.1, textShadow: "0px 0px 8px rgb(255,255,255", boxShadow: "0px 0px 8px rgb(255,255,255" }}
-
-            >Next</motion.button>
+              variants={buttonVariants}
+              whileHover="hover"
+            >
+              Next
+            </motion.button>
           </Link>
-        </div>
-      )
-      }
-
-    </motion.div >
+        </motion.div>
+      )}
+    </motion.div>
   )
 }
 
--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import {motion} from 'framer-motion';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: 5
+    }
+  }
+}
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { delay: 1.5, duration: 1.5 }
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: 'easeInOut' }
+  }
+};
 
 const Home = () => {
   return (
     <motion.div className="home container"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 1, duration: 3 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
     >
-      <motion.h2 animate={{}}>
-        Welcome to Pizza Joint
-      </motion.h2>
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
         <motion.button
-          animate={{
-          }}
-          whileHover={{ scale: 1.1, textShadow: "0px 0px 8px rgb(255,255,255", boxShadow: "0px 0px 8px rgb(255,255,255" }}>
+          variants={buttonVariants}
+          whileHover="hover"
+        >
           Create Your Pizza
         </motion.button>
       </Link>
